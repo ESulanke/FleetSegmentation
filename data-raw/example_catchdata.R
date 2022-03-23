ce5b4e19fde674314f780019f93c55aa4978c1da
Erik Sulanke <59560886+ESulanke@users.noreply.github.com>
2022-03-23 11:23:18 +0100

    Delete example_catchdata.R

--- data-raw/example_catchdata.R
+++
@@ -1,7 +0,0 @@
-## code to prepare `example_catchdata` and Mediterranean Stocks dataset goes here
-library(usethis)
-example_catchdata <- read.csv("data-raw\\Catchdata_Example.csv",sep=";",dec=",")
-use_data(example_catchdata, overwrite = TRUE)
-
-MED_stocks <- read.csv("data-raw\\MED_Stocks.csv")
-use_data(MED_stocks, overwrite = TRUE)
