7568ec7dbdf64acbb71ad80956c2ea0bc4d29a60
Erik Sulanke <59560886+ESulanke@users.noreply.github.com>
2022-03-24 13:13:34 +0100

    Add files via upload

---
+++ data-raw/example_lengthdata.R
@@ -0,0 +1,10 @@
+## code to prepare `example_shipdata` dataset goes here
+library(usethis)
+example_lengthdata <- read.csv("data-raw\\Shipdata_Example.csv",sep=";",dec=",")
+use_data(example_lengthdata, overwrite = TRUE)
+
+assemblage <- read.csv("data-raw\\assemblage.csv",sep = ";",dec = ",")
+use_data(assemblage, overwrite = TRUE)
+
+example_fleetdata <- read.csv("data-raw\\Fleetdata_Example.csv",sep = ";",dec = ",")
+use_data(example_fleetdata, overwrite = TRUE)
