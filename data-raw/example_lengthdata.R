c86a14a7bd595a0b3fa6ee4afbf8d7134847a1fd
Erik Sulanke <59560886+ESulanke@users.noreply.github.com>
2022-03-24 18:19:45 +0100

    Add files via upload

--- data-raw/example_lengthdata.R
+++ data-raw/example_lengthdata.R
@@ -6,5 +6,5 @@ use_data(example_lengthdata, overwrite = TRUE)
 assemblage <- read.csv("data-raw\\assemblage.csv",sep = ";",dec = ",")
 use_data(assemblage, overwrite = TRUE)
 
-example_fleetdata <- read.csv("data-raw\\Fleetdata_Example.csv",sep = ";",dec = ",")
+example_fleetdata <- read.csv("data-raw\\Fleetdata_Example.csv",sep = ",",dec = ".")
 use_data(example_fleetdata, overwrite = TRUE)
